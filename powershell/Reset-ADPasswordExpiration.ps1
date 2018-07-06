29fc7493e1d27e2545e09440a7276b36acaf53f2
Daniel Wood <daniel@woods.us>
2018-07-05 20:18:54 -0700

    Update Reset-ADPasswordExpiration.ps1

--- powershell/Reset-ADPasswordExpiration.ps1
+++ powershell/Reset-ADPasswordExpiration.ps1
@@ -1,26 +1,26 @@
 function Reset-ADPasswordExpiration {
     <#
     .SYNOPSIS
-    Resets AD Password Expiration by changing date of pwdlastset to +30days or now, whichever is older.
+    Resets AD Password Expiration by changing date of pwdlastset to now.
     .DESCRIPTION
-    Resets AD Password Expiration by changing date of pwdlastset to +30days or now, whichever is older.
+    Resets AD Password Expiration by changing date of pwdlastset to now.
     .NOTES
     Script by Daniel Wood (https://github.com/danielewood). Code is licened under Unlicense / CCZero / WTFPL / Public Domain.
     .LINK
     https://github.com/danielewood/misc/tree/master/powershell
     .EXAMPLE
-Get-ADUser -Filter {samaccountname -like 'dwoo*'} | Reset-ADPasswordExpiration -ResetExtensionDays 90 -Verbose
-    VERBOSE: CN=Daniel Wood,OU=Users,DC=contoso,DC=com
-    VERBOSE: Changed pwdlastset from 131752961323435741 to Now
+Get-ADUser -Filter {samaccountname -like 'dwoo*'} | Reset-ADPasswordExpiration -Verbose
+VERBOSE: $Identity = CN=Daniel Wood,OU=Users,DC=contoso,DC=com
+VERBOSE: Changed pwdlastset from 131753201305111407 to Now
+VERBOSE: Changed pwdlastsetDate from 07/06/2018 03:08:50 to Now
 
 
-    DistinguishedName : CN=Daniel Wood,OU=Users,DC=contoso,DC=com
-    SamAccountName    : dwood
-    pwdlastset        : 131752961867300958
-    pwdlastsetDate    : 7/5/2018 8:29:46 PM
-    OLDpwdlastset     : 131752961323435741
-    OLDpwdlastsetDate : 7/5/2018 8:28:52 PM
-
+DistinguishedName : CN=Daniel Wood,OU=Users,DC=contoso,DC=com
+SamAccountName    : dwood
+pwdlastset        : 131753201776369314
+pwdlastsetDate    : 7/6/2018 3:09:37 AM
+OLDpwdlastset     : 131753201305111407
+OLDpwdlastsetDate : 7/6/2018 3:08:50 AM
     #>
 
     [CmdletBinding()]
@@ -32,37 +32,31 @@ Get-ADUser -Filter {samaccountname -like 'dwoo*'} | Reset-ADPasswordExpiration -
         [alias("SamAccountName","DistinguishedName")]
         [string[]] $Identity,
         
-        [Parameter(Mandatory=$False,ValueFromPipeline=$False,HelpMessage="Number of days to extend expiration by")]
-        [int] $ResetExtensionDays = 30,
-
         [Parameter(Mandatory=$False)]
         [switch] $WhatIf
         ) # end param
     begin {}
     process {
-        foreach ($UserName in $Identity) {
-            Write-Verbose $UserName
-            $User = Get-ADUser -Identity "$UserName" -properties pwdlastset 
-            $ResetTime = [datetime]::FromFileTimeUTC($User.pwdlastset).AddDays($ResetExtensionDays).ToFileTimeUTC()
-            $Oldpwdlastset = $User.pwdlastset
-            if ($ResetTime -lt [datetime]::Now.ToFileTimeUTC()){
-                $User.pwdlastset = $ResetTime
-                Set-ADUser -Instance $User -WhatIf:($WhatIf)
-                Write-Verbose "Changed pwdlastset from $Oldpwdlastset to $($User.pwdlastset)"
-            } else {
-                $User.pwdlastset = 0 
-                Set-ADUser -Instance $User -WhatIf:($WhatIf)
-                $User.pwdlastset = -1 
-                Set-ADUser -instance $User -WhatIf:($WhatIf)
-                Write-Verbose "Changed pwdlastset from $Oldpwdlastset to Now"
-            }
-            Get-ADUser -Identity "$UserName" -properties pwdlastset | 
-                Select DistinguishedName, SamAccountName, pwdlastset,
-                @{ Label=”pwdlastsetDate”; Expression={[datetime]::FromFileTimeUTC($_.pwdlastset)} },
-                @{ Label=”OLDpwdlastset”; Expression={$Oldpwdlastset} },
-                @{ Label=”OLDpwdlastsetDate”; Expression={[datetime]::FromFileTimeUTC($Oldpwdlastset)} } 
-        } # end foreach
+        $User = Get-ADUser -Identity "$Identity" -properties pwdlastset 
+        $Oldpwdlastset = $User.pwdlastset
 
+        Write-Verbose "`$Identity = $Identity"    
+		Write-Debug "`$User = $User"
+        Write-Debug "`$Oldpwdlastset = $Oldpwdlastset"
+
+        $User.pwdlastset = 0 
+        Set-ADUser -Instance $User -WhatIf:($WhatIf)
+        $User.pwdlastset = -1 
+        Set-ADUser -instance $User -WhatIf:($WhatIf)
+        
+        Write-Verbose "Changed pwdlastset from $Oldpwdlastset to Now"
+        Write-Verbose "Changed pwdlastsetDate from $([datetime]::FromFileTimeUTC($Oldpwdlastset)) to Now"
+        
+        Get-ADUser -Identity "$Identity" -properties pwdlastset | 
+            Select DistinguishedName, SamAccountName, pwdlastset,
+            @{ Label=”pwdlastsetDate”; Expression={[datetime]::FromFileTimeUTC($_.pwdlastset)} },
+            @{ Label=”OLDpwdlastset”; Expression={$Oldpwdlastset} },
+            @{ Label=”OLDpwdlastsetDate”; Expression={[datetime]::FromFileTimeUTC($Oldpwdlastset)} } 
     } # end process
     end {}
 } # end function 
