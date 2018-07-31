17b79211cc2ceb272e0347eb8cb0cf53b78a8a08
Daniel Wood <daniel@woods.us>
2018-07-31 16:45:21 -0700

    Update Convert-AcmeCERtoPFX.sh

--- shell/Convert-AcmeCERtoPFX.sh
+++ shell/Convert-AcmeCERtoPFX.sh
@@ -10,6 +10,7 @@ PFX_PASSWORD='P@S5W0rd!'
 #Begin Script
 CERT_HOME=`eval echo $CERT_HOME`
 BLUE='\033[0;34m'
+RED='\033[0;31m'
 NC='\033[0m' # No Color
 while read -r config; do
 	CERT_PATH="`echo $config | sed 's%/[^/]*$%%'`"
@@ -20,9 +21,9 @@ while read -r config; do
 	CERT_FRIENDLYNAME="$CERT_CN-$CERT_NOTBEFORE-$CERT_NOTAFTER"
 
 	printf "${BLUE}---${NC}\n"
-	[ ! $CERT_NOTAFTER ] && echo "Could not evaluate certificate for $CERT_PATH/$CERT_CN.cer, skipping..." && continue
+	[ ! $CERT_NOTAFTER ] && printf "Could not evaluate certificate for {RED}$CERT_PATH/$CERT_CN.cer${NC}, skipping...\r\n" && continue
+	[ -f $CERT_PATH/$CERT_FRIENDLYNAME.pfx ] && printf "${RED}$CERT_PATH/$CERT_FRIENDLYNAME.pfx${NC} already exists, skipping.\r\n" && continue
 
-	rm -f $CERT_PATH/$CERT_FRIENDLYNAME.pfx &>/dev/null
 	openssl pkcs12 -inkey $CERT_PATH/$CERT_CN.key -in $CERT_PATH/$CERT_CN.cer -certfile $CERT_PATH/fullchain.cer -name $CERT_FRIENDLYNAME -export -out $CERT_PATH/$CERT_FRIENDLYNAME.pfx -password pass:"$PFX_PASSWORD"
 	openssl pkcs12 -info -in $CERT_PATH/$CERT_FRIENDLYNAME.pfx -password pass:$PFX_PASSWORD -nokeys -clcerts 2>/dev/null grep '^subject='
 	CERT_SANS="`openssl x509 -in $CERT_PATH/$CERT_CN.cer -noout -text | grep -A1 'Subject Alternative Name' | tail -1 | tr -d ' ' | sed 's/DNS:/\t/' | sed 's/\,DNS:/\r\n\t/g'`"
@@ -32,4 +33,5 @@ while read -r config; do
 	[ "$CERT_SANS" ] && printf "Subject Alternative Names:\r\n$CERT_SANS\r\n"
 	echo "Path to certificates: $CERT_PATH"
 	echo "PFX is located at: `ls $CERT_PATH/$CERT_FRIENDLYNAME.pfx`"
+	echo "`openssl x509 -in $CERT_PATH/$CERT_CN.cer -noout -text`" > $CERT_PATH/$CERT_FRIENDLYNAME.pfx.txt
 done <<< "`grep -rl 'Le_Domain=' $CERT_HOME | grep '\.conf$'`"
