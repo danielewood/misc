c0326c842a37a85d19c6ba5ff5cfc4accf67f08b
Daniel Wood <daniel@woods.us>
2018-07-31 16:25:23 -0700

    Update Convert-AcmeCERtoPFX.sh

--- shell/Convert-AcmeCERtoPFX.sh
+++ shell/Convert-AcmeCERtoPFX.sh
@@ -19,11 +19,14 @@ while read -r config; do
 	CERT_NOTAFTER="$(date -d "`openssl x509 -in $CERT_PATH/$CERT_CN.cer -noout -text | grep 'Not After :' | awk -F'After : ' '{print $2}'`" +"%Y%m%d")"
 	CERT_FRIENDLYNAME="$CERT_CN-$CERT_NOTBEFORE-$CERT_NOTAFTER"
 
+	printf "${BLUE}---${NC}\n"
+	[ ! $CERT_NOTAFTER ] && echo "Could not evaluate certificate for $CERT_PATH/$CERT_CN.cer, skipping..." && continue
+
 	rm -f $CERT_PATH/$CERT_FRIENDLYNAME.pfx &>/dev/null
 	openssl pkcs12 -inkey $CERT_PATH/$CERT_CN.key -in $CERT_PATH/$CERT_CN.cer -certfile $CERT_PATH/fullchain.cer -name $CERT_FRIENDLYNAME -export -out $CERT_PATH/$CERT_FRIENDLYNAME.pfx -password pass:"$PFX_PASSWORD"
 	openssl pkcs12 -info -in $CERT_PATH/$CERT_FRIENDLYNAME.pfx -password pass:$PFX_PASSWORD -nokeys -clcerts 2>/dev/null grep '^subject='
 	CERT_SANS="`openssl x509 -in $CERT_PATH/$CERT_CN.cer -noout -text | grep -A1 'Subject Alternative Name' | tail -1 | tr -d ' ' | sed 's/DNS:/\t/' | sed 's/\,DNS:/\r\n\t/g'`"
-	printf "${BLUE}---${NC}\n"
+
 	echo "Friendly Name: $CERT_FRIENDLYNAME"
 	echo "Common Name = $CERT_CN"
 	[ "$CERT_SANS" ] && printf "Subject Alternative Names:\r\n$CERT_SANS\r\n"
