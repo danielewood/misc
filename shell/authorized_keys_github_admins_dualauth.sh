5cd2b2d0df929d7f92087b0f3bf5e16dd88668ec
Daniel Wood <daniel@woods.us>
2019-07-29 22:30:12 -0700

    Style cleanup

--- shell/authorized_keys_github_admins_dualauth.sh
+++ shell/authorized_keys_github_admins_dualauth.sh
@@ -2,8 +2,6 @@
 # If a Public SSH Key (ed25519) of an Organization Admin appears in multiple Admin accounts, publish to /root/.ssh/authorized_keys_github_admins_dualauth
 # Run periodically as a cron job, frequency of cron job will determine how long a key is live for.
 
-# var=$'' injects excape sequences into variables
-
 # GITHUB_USERNAME GitHubAPI-READMEMBERS-SSHAutomation personal access token
 #   Permissions: read:gpg_key, read:org, read:public_key
 github_api_username='GITHUB_USERNAME'
@@ -14,6 +12,7 @@ github_users=$(curl -u $github_api_username:$github_api_key https://api.github.c
 
 for github_user in $github_users; do
   authorized_keys+="$(curl https://github.com/"$github_user".keys 2>/dev/null | grep -E '^ssh-ed25519' )"
+  # var=$'' injects excape sequences into variables
   authorized_keys+=$'\r\n'
 done
 
