5e67578aedffae0822ec89eaa612d4a2dc86e001
Alex Turner <alex.turner.pro@gmail.com>
2020-04-13 18:16:30 -0400

    Create Plex-Argo-DirectoryUpdate-systemd.bash
    Per @danielewood request: create a systemd file

---
+++ plex-argo/Plex-Argo-DirectoryUpdate-systemd.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Assumes the cloudflared is running with metrics server
+# Example: cloudflared tunnel --url 'http://localhost:32400' --metrics 'localhost:33400'
+
+PreferencesPath='/var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Preferences.xml'
+
+PlexOnlineToken='https://plex.tv/api/resources?X-Plex-Token='$(grep -oP 'PlexOnlineToken="\K[^"]*' "${PreferencesPath}")
+PlexAPIcustomConnections=$(curl -s $PlexOnlineToken | grep -oP 'address="\K[^"]*\.trycloudflare\.com' | head -n1)
+
+ArgoURL=$(curl -s http://localhost:33400/metrics | grep -oP 'userHostname="https://\K[^"]*\.trycloudflare\.com' | head -n1)
+
+[ -z $ArgoURL ] && exit
+[ -z $PlexAPIcustomConnections ] && exit
+
+if [[ $ArgoURL != $PlexAPIcustomConnections ]]; then
+    systemctl stop plexmediaserver.service
+    PreferencesValue="https://${ArgoURL}:443,http://${ArgoURL}:80"
+    # Set new Argo URL
+    sed -i "s|customConnections=\".*\"|customConnections\=\"${PreferencesValue}\"|" "${PreferencesPath}"
+    # Disable Plex Relay Servers
+    sed -i "s|RelayEnabled=\"1\"|RelayEnabled=\"0\"|" "${PreferencesPath}"
+    # Disable Plex Remote Access Methods
+    sed -i "s|PublishServerOnPlexOnlineKey=\"1\"|PublishServerOnPlexOnlineKey=\"0\"|" "${PreferencesPath}"
+    systemctl start plexmediaserver.service
+fi
